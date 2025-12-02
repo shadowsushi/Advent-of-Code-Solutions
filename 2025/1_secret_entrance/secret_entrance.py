b1e761444b8e8d2fd6b3f65fd8664813f62b295f
shadowsushi <winsyew71@gmail.com>
2025-12-02 15:09:38 -0500

    Complete day 1 solution

--- 2025/1_secret_entrance/secret_entrance.py
+++ 2025/1_secret_entrance/secret_entrance.py
@@ -1,7 +1,7 @@
 # Initialize variables
 initial_knob_position = 50
 list_of_rotations = []
-input_file = 'input.txt'
+input_file = '2025/1_secret_entrance/input.txt'
 
 
 def read_rotations_from_file(input_file):
@@ -26,17 +26,30 @@ def calculate_times_at_zero(initial_position, list_of_rotations):
 
 
 def calculate_anytime_at_zero(initial_position, list_of_rotations):
-    position = initial_position
+    current_position = initial_position
     times_at_zero = 0
     for rotation in list_of_rotations:
-        position += rotation
-        if abs(position) // 100 > 0 and position != 0:
-            times_at_zero = times_at_zero + (abs(position) // 100)
-            position = position % 100
+        new_position = current_position + rotation
+        # print('---')
+        # print(f'{current_position} moving {rotation} to {new_position}')
+        if current_position == 0:
+            times_at_zero += abs(new_position) // 100
+        else:
+            if new_position <= 0:
+                times_at_zero += (abs(new_position) // 100) + 1
+            elif new_position // 100 > 0:
+                times_at_zero += abs(new_position) // 100
+        current_position = new_position % 100 # Convert new position to real dial position
+        # print(f'new dial position: {current_position}')
+        # print(f'current times at zero: {times_at_zero}')
     return times_at_zero
 
 
-if __name__ == "__main__":
+def main():
     read_rotations_from_file(input_file)
     print(f'The password is: {calculate_times_at_zero(initial_knob_position, list_of_rotations)}')
     print(f'The method 0x434C49434B password is: {calculate_anytime_at_zero(initial_knob_position, list_of_rotations)}')
+
+
+if __name__ == "__main__":
+    main()
