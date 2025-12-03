a85eb1f0ed2f1c859eea3de2a2d4898a7c757f07
shadowsushi <winsyew71@gmail.com>
2025-12-02 22:49:03 -0500

    Start day 2 part 2 solution

--- 2025/2_id_database/id_database.py
+++ 2025/2_id_database/id_database.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 invalid_ids_sum = 0
-file_path = 'input.txt'
+file_path = '2025/2_id_database/input.txt'
 
 
 def read_id_database(file_path):
@@ -13,24 +13,24 @@ def read_id_database(file_path):
     return list_of_start_and_end_points # Returns list of [start, end] ID ranges
 
 
-def get_ids_in_range(start_id, end_id):
+def get_IDs_in_range(start_id, end_id):
     IDs = []
     for i in range(start_id, end_id + 1):
         IDs.append(i)
     return IDs
 
 
-def the_function_that_takes_the_list_of_start_and_end_points(list_of_start_and_end_points):
+def get_all_IDs(list_of_start_and_end_points):
     all_IDs = []
     for start_end in list_of_start_and_end_points:
         start_id = int(start_end[0])
         end_id = int(start_end[1])
-        ids_in_range = get_ids_in_range(start_id, end_id)
+        ids_in_range = get_IDs_in_range(start_id, end_id)
         all_IDs.extend(ids_in_range)
     return all_IDs
 
 
-def check_duplicate_portion(id_string):
+def check_single_duplicate_portion(id_string):
     first_portion = id_string[0:len(id_string)//2]
     second_portion = id_string[len(id_string)//2:]
     if first_portion == second_portion:
@@ -39,14 +39,23 @@ def check_duplicate_portion(id_string):
         return
 
 
-def main():
+def invalid_ids_sum_part_one():
     list_of_start_and_end_points = read_id_database(file_path)
-    all_IDs = the_function_that_takes_the_list_of_start_and_end_points(list_of_start_and_end_points)
+    all_IDs = get_all_IDs(list_of_start_and_end_points)
     for ID in all_IDs:
         global invalid_ids_sum
-        if check_duplicate_portion(str(ID)):
+        if check_single_duplicate_portion(str(ID)):
             invalid_ids_sum += ID
-    print(invalid_ids_sum)
+    return invalid_ids_sum
+
+
+def invalid_ids_sum_part_two():
+    return
+
+
+def main():
+    print(f'The sum of the invalid IDs with single duplicates is: {invalid_ids_sum_part_one()}')
+    print(f'The sum of the invalid IDs with multiple duplicates is: {invalid_ids_sum_part_two()}')
 
 
 if __name__ == '__main__':
